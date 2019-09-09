75bccdb767f7adf5237d2f497822477b55894da9
Nervado <evandroabreudeoliveira@gmail.com>
2019-09-09 06:42:47 -0300

    intial

---
+++ .next/static/webpack/static/development/pages/index.js.7246df17238daa135394.hot-update.js
@@ -0,0 +1,36 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/home/nervado/Documents/Dev/bootcamp/nextjs/pages/index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+var Home = function Home() {
+  return __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Hello Next!");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.7246df17238daa135394.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+const Home = () => (
+  <div>
+    <h1>Hello Next!</h1>
+
+
+
+
+    <Link href="/users">Usuários</Link>
+  </div>
+
+
+
+
+
+);
+export default Home;
+//Este é um comentário usando vim 
+
+//Este é um outro comentário editado usando vim 
